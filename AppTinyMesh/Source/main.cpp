60ae00360d22faee2ad4d0dac30aba7501843c30
Stanislas Bagnol <stanislas.bagnol@etu.univ-lyon1.fr>
2022-10-24 16:29:02 +0200

    Ajout de disc.h et cone.h dans le CMakeLists.txt

--- AppTinyMesh/Source/main.cpp
+++ AppTinyMesh/Source/main.cpp
@@ -9,8 +9,8 @@ int main(int argc, char *argv[])
 
     m.SaveObj(QString("objet.obj"), QString("cube"));
 
-	MainWindow mainWin;
-	mainWin.showMaximized();
+	//MainWindow mainWin;
+	//mainWin.showMaximized();
 	//Mesh testObject = Mesh(Cylinder(Vector(-1.0, 0.0, -1.0), Vector(0.0, 0.0, 1.0), 1.0), 64);
 	//Mesh testObject = Mesh(Cylinder(Vector(-1.0, 0.0, -1.0), 5.0, 1.0), 64);
 	//testObject.SaveObj("testObject.obj","testObject");
